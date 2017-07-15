cebb504e626a575a4ee75e8e952662c97038d6eb
Chris Klimowski <csklimowski@gmail.com>
2017-07-14 23:16:19 -0700

    added loading text

--- src/states/load.js
+++ src/states/load.js
@@ -2,6 +2,7 @@ import game from '../jetpack';
 
 export default class LoadState extends Phaser.State {
 	preload() {
+		var loadingText = game.add.text(158, 150, 'Loading...', { font: '15px monospace', fill: '#00ff00', align: 'center'});
 		game.load.spritesheet('player', 'img/player.png', 32, 43);
 		game.load.spritesheet('background', 'img/bg.png', 315, 1000);
 		game.load.spritesheet('hoverer', 'img/hoverer.png', 48, 60);
@@ -12,8 +13,8 @@ export default class LoadState extends Phaser.State {
 		game.load.spritesheet('shards', 'img/shards.png', 24, 24);
 		game.load.spritesheet('bullet', 'img/bullet.png', 15, 10);
 		game.load.spritesheet('arrows', 'img/arrows.png', 76, 76);
-		game.load.image('smoke', 'img/smoke.png', 35, 35);
-		game.load.image('triangle', 'img/triangle.png', 50, 50);
+		game.load.image('smoke', 'img/smoke.png');
+		game.load.image('triangle', 'img/triangle.png');
 		game.load.image('fire', 'img/fire.png');
 		game.load.image('display', 'img/display.png');
 	}
